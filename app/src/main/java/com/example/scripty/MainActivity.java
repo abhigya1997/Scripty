8c435d6b92291f5937c89312099ed0ddbd5c44c2
abhigya1997 <abhibhar1697@gmail.com>
2020-07-29 15:47:18 +0530

    first commit

---
+++ app/src/main/java/com/example/scripty/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.scripty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, content.class));
+            }
+        });
+
+
+    }
+}
